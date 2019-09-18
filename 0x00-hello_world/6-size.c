ebd3515de3f886a2c513aa8719e59fdecc376c74
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-18 14:07:06 +0100

    Even more fixes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@ long long llongintType;
 float floatType;
 char charType;
 
-printf("Size of char: %ld byte\n", sizeof(charType));
-printf("Size of int: %ld bytes\n", sizeof(integerType));
-printf("Size of long int: %ld bytes\n", sizeof(longintType));
-printf("Size of long long int: %ld bytes\n", sizeof(llongintType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
+printf("Size of char: %d byte\n", sizeof(charType));
+printf("Size of int: %d bytes\n", sizeof(integerType));
+printf("Size of long int: %d bytes\n", sizeof(longintType));
+printf("Size of long long int: %d bytes\n", sizeof(llongintType));
+printf("Size of float: %d bytes\n", sizeof(floatType));
 return (0);
 }
