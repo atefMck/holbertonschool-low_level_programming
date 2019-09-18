10d512cf95215d3157613da4384068bd99d4597a
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-18 14:09:19 +0100

    Even more fixes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-int integerType;
-long longintType;
-long long llongintType;
-float floatType;
-char charType;
-
-printf("Size of char: %d byte\n", sizeof(charType));
-printf("Size of int: %d bytes\n", sizeof(integerType));
-printf("Size of long int: %d bytes\n", sizeof(longintType));
-printf("Size of long long int: %d bytes\n", sizeof(llongintType));
-printf("Size of float: %d bytes\n", sizeof(floatType));
+printf("Size of char: %d byte\n", sizeof(char));
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of long int: %d bytes\n", sizeof(long));
+printf("Size of long long int: %d bytes\n", sizeof(long long));
+printf("Size of float: %d bytes\n", sizeof(float));
 return (0);
 }
