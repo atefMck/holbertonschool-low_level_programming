0f8d6db1f012587a77bd67a9bb521a3eb46b74b2
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-19 15:31:59 +0100

    Please work checker

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * putchar a-z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a >= 102; a++)
+{
+putchar(a);
+if (a == 57)
+a = 97;
+}
+putchar('\n');
+return (0);
+}
