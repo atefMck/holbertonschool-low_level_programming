eeaf21633c40ab8409ee260f07fee45eaebc04b0
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-19 16:01:54 +0100

    qsdsq

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * putchar a-z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int a,b;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+putchar(a);
+putchar(b);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
