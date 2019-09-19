38545ebef77a1150ebe7280e4a020d46e92a8d99
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-19 15:09:44 +0100

    64151 try

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 int a;
-for (a = 97; a <= 122; a++)
-putchar(a);
-a = 65;
-for (a = 65; a <= 90; a++)
-putchar(a);
-putchar("\n");
+
+for (a = 97 ; a <= 122 ; a++)
+putchar (a);
+for (a = 65 ; a <= 90 ; a++)
+putchar (a);
+putchar('\n');
+return (0);
 }
