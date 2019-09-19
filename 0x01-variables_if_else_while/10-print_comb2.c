20268de885b725f922d1a082d223d03f174c20b7
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-19 16:06:29 +0100

    qsdsq

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,15 +6,18 @@
  */
 int main(void)
 {
-  int a,b;
+int a,b;
 for (a = '0'; a <= '9'; a++)
 {
 for (b = '0'; b <= '9'; b++)
 {
 putchar(a);
 putchar(b);
+ if (!((a == '9') && (b == '9')))
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
