1c418ebcb7b1719f745c63a6643484c45877de28
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-19 14:00:21 +0100

    lkl

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* A comment for betty */
+/**
+ * main - Entry point
+ * putchar a-z
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int a;
@@ -8,4 +12,5 @@ for (a = 97; a < 122; a++)
 putchar(a);
 }
 putchar('\n');
+return (0);
 }
