1dafe57b512c526b7f8914334eda17f950dd0ce5
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-24 11:07:42 +0100

    fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
-
+/**
+ * print_alphabet - entry point
+ * print alphabet
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
 int i, j;
 for (j = 0; j < 10; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
-{
 _putchar(i);
-}
 _putchar('\n');
 }
 }
