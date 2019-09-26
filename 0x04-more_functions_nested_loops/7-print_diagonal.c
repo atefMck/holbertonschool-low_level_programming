72719f461b091e626a0e828e6d34dedd84e33478
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-26 15:20:53 +0100

    lol

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,21 @@
  */
 void print_diagonal(int n)
 {
-  int i,j;
-  for(i = 1; i <= n; i++)
-    {
-      for(j = 0; j <= i; j++)
-	{
-	  if (j == i)
-	    _putchar('\\');
-	  else
-	    _putchar(' ');
-	}
-      _putchar('\n');
-    }
+int i,j;
+if (n > 0)
+{
+for(i = 1; i <= n; i++)
+{
+for(j = 0; j <= i; j++)
+{
+if (j == i)
+_putchar('\\');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
