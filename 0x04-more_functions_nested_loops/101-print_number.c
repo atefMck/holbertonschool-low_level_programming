fb2bda3099f22f732a7f0d32f449e285896aa911
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-26 16:32:17 +0100

    loooool

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_number - print a number.
+ * @n: the number to print
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+if (n >= 0)
+{
+while (n >= 0)
+{
+_putchar(n % 10 + '0');
+n /= 10;
+if (n == 0)
+n = n - 1;
+}
+}
+else
+{
+n *= -1;
+_putchar('-');
+while (n != 0)
+{
+_putchar(n % 10 + '0');
+n /= 10;
+}
+}
+}
