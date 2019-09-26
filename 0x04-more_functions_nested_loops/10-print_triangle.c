198e706840b20792947da4ba2f0f32346f52de95
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-26 15:35:14 +0100

    looooool

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,6 +7,8 @@
 void print_triangle(int size)
 {
 int i,j;
+if (size > 0)
+{
 for(i = size; i > 0; i--)
 {
 for(j = 1; j <= size; j++)
@@ -18,4 +20,7 @@ _putchar(' ');
 }
 _putchar('\n');
 }
+}
+else
+_putchar('\n');
 }
