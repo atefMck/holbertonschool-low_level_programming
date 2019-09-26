d679890b6b7dff704b0da21732a3a1c27bc644bd
Atef Mechken <atef.mechken@youngminds.tn>
2019-09-26 16:13:53 +0100

    lol

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - write biggest factor
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long num = 612852475143;
+long i;
+for (i = 2; i <= num; i++)
+{
+if (num % i == 0)
+{
+while (num % i == 0)
+num = num / i;
+}
+}
+printf("%ld\n", i);
+return (0);
+}
