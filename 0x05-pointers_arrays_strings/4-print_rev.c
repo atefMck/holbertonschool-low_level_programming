79fc6a3742b950ce6133baf96623ed1049666935
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-01 11:36:00 +0100

    lol

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_rev - prints a string in reverse
  * @s: the string to print in reverse
@@ -9,11 +8,7 @@ void print_rev(char *s)
 {
 int i;
 for (i = 0; *(s + i); i++);
-char c;
 for (; i > 0; i--)
-{
-c = *(s + i - 1);
-_putchar(c);
-}
+_putchar(*(s + i - 1));
 _putchar('\n');
 }
