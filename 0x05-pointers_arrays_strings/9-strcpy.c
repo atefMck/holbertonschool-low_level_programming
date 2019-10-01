8c56aeb8a13c16db7f8a1d4fc1c6a1cb0767c00a
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-01 11:19:43 +0100

    lololol

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,9 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-while (*(src + i))
+for (i = 0; *(src + i); i++)
 {
 *(dest + i) = *(src + i);
-i++;
 }
-*(src + i + 1) = '\0';
 return (dest);
 }
