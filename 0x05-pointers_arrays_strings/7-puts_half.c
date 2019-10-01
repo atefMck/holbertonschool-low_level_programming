ff8d567874b55c450e93cbe6ab540c59d0920e69
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-01 11:56:09 +0100

    ya rab

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,9 +7,8 @@
 void puts_half(char *str)
 {
 int i = 0;
-while (*(str + i))
+while (str[i])
 i++;
-char c;
 int j = i;
 if (j % 2 != 0)
 j = i / 2 + 1;
@@ -17,8 +16,7 @@ else
 j = i / 2;
 for (; j <= i; j++)
 {
-c = *(str + j);
-_putchar(c);
+_putchar(str[j]);
 }
 _putchar('\n');
 }
