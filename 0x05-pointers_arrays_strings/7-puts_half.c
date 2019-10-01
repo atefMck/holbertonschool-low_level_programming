944ce986cc5ad546bd220f9e9b888aae12033a33
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-01 12:11:32 +0100

    lol

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,17 @@
  */
 void puts_half(char *str)
 {
-int i;
+int i,j,k;
 for (i = 0; str[i]; i++)
 ;
-int j;
-if (i % 2 != 0)
-j = (i - 1) / 2;
-else
-j = i / 2;
-for (; j < i; j++)
+k = i;
+i /= 2;
+if (k % 2 != 0)
+k++;
+for (j = 0; j < k; j++)
+{
+if (j >= i)
 _putchar(str[j]);
+}
 _putchar('\n');
 }
