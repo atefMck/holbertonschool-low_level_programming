f0ac47138753c06111334196f89a42c4b2201bc5
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-01 11:15:34 +0100

    lololol

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * _strcpy - copies string to another
- * @dest: the string to copy in 
- * @src: the string to copy from 
+ * @dest: the string to copy in
+ * @src: the string to copy from
  * Return: dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-while(*(src + i))
+int i = 0;
+while (*(src + i))
 {
 *(dest + i) = *(src + i);
 i++;
