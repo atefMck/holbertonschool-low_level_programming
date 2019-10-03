98794000c8f23cc7621b6170c9e1b836c796a843
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-03 14:08:16 +0100

    YEET!

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
-* cap_string - copie n bytes of a string
+* leet - copie n bytes of a string
 * @s: The destination string
 * Return: s.
 */
 char *leet(char *s)
 {
 int i, j;
-char a[]= {'A','E','O','T','L'};
-char b[]= {'a','e','o','t','l'};
-char c[]= {'4','3','0','7','1'};
+char a[] = {'A', 'E', 'O', 'T', 'L'};
+char b[] = {'a', 'e', 'o', 't', 'l'};
+char c[] = {'4', '3', '0', '7', '1'};
 for (i = 0; s[i]; i++)
 {
 for (j = 0; j <= 5; j++)
