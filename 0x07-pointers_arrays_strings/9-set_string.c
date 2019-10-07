563689c9f15945d93c455f4f0505d0481ba0e936
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-07 21:56:42 +0100

    lol

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* set_string - fills memory with a constant byte.
+* @s: the memory area to print in
+* @to: the memory area to print in
+* Return: void.
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
