10b80ee364b736035d2ec1cb78009cdf7f1a902a
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-09 15:55:52 +0100

    what the

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,23 +2,30 @@
 #include <stdio.h>
 
 /**
-* _pow_recursion - fills memory with a constant byte.
+* is_prime_number - fills memory with a constant byte.
 * @n: the memory area to print in
 * Return: void.
 */
 
 int is_prime_number(int n)
 {
-  if (n <= 1)
-    return (0);
-  return (prime(n, n / 2));
+if (n <= 1)
+return (0);
+return (prime(n, n / 2));
 }
+
+/**
+* prime - fills memory with a constant byte.
+* @a: the memory area to print in
+* @b: the memory area to print in
+* Return: int
+*/
 
 int prime(int a, int b)
 {
-  if (a % b == 0)
-  return (0);
-  else
-  prime(a, b - 1);
-  return (1);
+if (a % b == 0)
+return (0);
+else
+prime(a, b - 1);
+return (1);
 }
