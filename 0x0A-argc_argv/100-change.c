01a90c28c3985b4bfd8e0192b7a5a20e0f8b7ec7
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-14 15:52:30 +0100

    adv done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - print numb of args.
+* @argc: number of args
+* @argv: the actual arguments
+* Return: Void.
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+return (0);
+}
+int change = atoi(argv[1]);
+int pieces = 0;
+while (change >= 25)
+{
+  change -= 25;
+  pieces++;
+}
+while (change >= 10)
+{
+  change -= 10;
+  pieces++;
+}
+while (change >= 5)
+{
+  change -= 5;
+  pieces++;
+}
+while (change >= 2)
+{
+  change -= 2;
+  pieces++;
+}
+while (change >= 1)
+{
+  change -= 1;
+  pieces++;
+}
+printf("%d\n", pieces);
+}
