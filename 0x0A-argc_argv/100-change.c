7dfa9500c8dcc40f1e84ccf3543074506ea803b6
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-14 16:56:01 +0100

    work please

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,17 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-
+int change = atoi(argv[1]);
 int pieces = 0;
 int coins[5] = {25, 10, 5, 2, 1};
 int i = 0;
+
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
-int change = atoi(argv[1]);
 
 if (change <= 0)
 {
