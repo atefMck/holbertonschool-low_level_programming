aa68e7839aa35d82d9fde85cf83849d458accef3
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-16 12:45:41 +0100

    fixxx

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,18 @@
 
 char *_strdup(char *str)
 {
+int i = 0;
+char *r;
+int size = 0;
 if (*str == '\0')
 return (NULL);
 
-int size = 0;
-while (str[size])
+while (str[size] != '\0')
 size++;
 
-int i = 0;
-char *r;
 r = malloc(sizeof(char) * size + 1);
-if (r == NULL)
-return (NULL);
+if (r == '\0')
+return (0);
 
 for (i = 0; i < size; i++)
 r[i] = str[i];
