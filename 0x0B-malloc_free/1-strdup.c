90b1cf10b1dae4a7d5b9b3fa2ef2ab1bf47ff9c3
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-16 12:33:39 +0100

    fix 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,7 +10,7 @@
 char *_strdup(char *str)
 {
 if (sizeof(str) == 0)
-return NULL;
+return (NULL);
 else
 {
 
@@ -22,12 +22,15 @@ size++;
 unsigned int i = 0;
 char *r;
 r = malloc(sizeof(char) * size);
+if (r == NULL)
+return (NULL);
 
 while (i < size)
 {
 r[i] = str[i];
 i++;
 }
+r[i] = '\0';
 
 return (r);
 }
