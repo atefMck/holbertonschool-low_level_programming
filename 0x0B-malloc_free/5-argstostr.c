c4bb59a3c0556d96261348e666d89d2a0b59bc44
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-16 14:52:54 +0100

    5 lol

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* argstostr - create an array with init
+* @ac: size of the array.
+* @av: size of the array.
+* Return: char
+*/
+
+char *argstostr(int ac, char **av)
+{
+char *r;
+int i, j, k = 0;
+if (ac == 0 || av == NULL)
+return NULL;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+k++;
+}
+r = malloc(sizeof(char) * k + 1 + ac);
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+r[k] = av[i][j];
+k++;
+}
+r[k] = '\n';
+k++;
+}
+return (r);
+}
