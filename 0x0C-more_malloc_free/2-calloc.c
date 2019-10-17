a13ed47dd3f517e5103f769de00be06b1f883206
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-17 21:01:40 +0100

    lollll

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,14 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *r;
+char *r;
+unsigned int i;
 if (nmemb <= 0 || size <= 0)
 return (NULL);
 r = malloc(size * nmemb);
 if (r == NULL)
 return (NULL);
+for (i = 0; i < nmemb * size; i++)
+r[i] = 0;
 return (r);
 }
