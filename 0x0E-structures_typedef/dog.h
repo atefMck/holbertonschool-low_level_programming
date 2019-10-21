c36c5ad42892ef99944c140ff31bf0f8404eeb02
atefsama666 <54045904+atefsama666@users.noreply.github.com>
2019-10-22 00:35:48 +0200

    Doggo in place

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOGGO_
+#define _DOGGO_
+/**
+ * struct dog - Defines a dogs attributes
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the owners name
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} 
+#endif
