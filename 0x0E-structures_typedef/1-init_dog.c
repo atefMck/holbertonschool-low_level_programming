1c350e1a222a611966f7f927719419f0618ad989
atefsama666 <54045904+atefsama666@users.noreply.github.com>
2019-10-22 00:40:55 +0200

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - yes
+ * @d: pointer to  dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner f the dog
+ * Return void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+*(d).name = name;
+*(d).age = age;
+*(d).owner = owner;
+}
+}
