e3938779f490fd30985560df2a0fce75b7402ae2
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-23 15:32:08 +0100

    ya rab

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+* main - main calculator
+* @argc: num of argument
+* @argv: array of argument
+* Return: pointer to corresponding function.
+*/
+
+int main(int argc, char *argv[])
+{
+
+int (*operation)(int, int);
+int r;
+int a;
+int b;
+operation = get_op_func(argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (operation == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if (b == 0 && (operation == op_div || operation == op_mod))
+{
+printf("Error\n");
+exit(100);
+}
+
+r = (*operation)(a, b);
+printf("%d\n", r);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+
+/**
+  * op_add - adittion
+  * @a: first num
+  * @b: second num
+  * Return: result.
+  */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+  * op_sub - substitution
+  * @a: first num
+  * @b: second num
+  * Return: result.
+  */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+  * op_mul - multiplication
+  * @a: first num
+  * @b: second num
+  * Return: result.
+  */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+  * op_div - division
+  * @a: first num
+  * @b: second num
+  * Return: result.
+  */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+  * op_mod - modulo
+  * @a: first num
+  * @b: second num
+  * Return: result.
+  */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
