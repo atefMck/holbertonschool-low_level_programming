8d25212401058375d85ddc5d07363ec008b708f1
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-23 12:07:48 +0100

    hello

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+size_t i;
+
 if (action == NULL)
 return;
 if (array == NULL)
 return;
 
-size_t i;
 for (i = 0; i < size; i++)
 (*action)(array[i]);
 }
