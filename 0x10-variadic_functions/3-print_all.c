3a08fe7b1a0c95e5fad4c2d5ea4203bddb626ea2
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-27 14:51:12 +0100

    second try

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,17 +8,6 @@ void printFloat(va_list lol);
 void printString(va_list lol);
 
 /**
-* form - struct
-* @format: the format
-* Return: void
-*/
-
-typedef struct form {
-  char cond;
-  void (*printType)(va_list lol);
-} form;
-
-/**
 * print_all - prints all arguments
 * @format: the format
 * Return: void
@@ -27,11 +16,11 @@ typedef struct form {
 void print_all(const char * const format, ...)
 {
 form mind[] = {
-  {'c', printChar},
-  {'i', printInt},
-  {'f', printFloat},
-  {'s', printString},
-  {0, NULL},
+{'c', printChar},
+{'i', printInt},
+{'f', printFloat},
+{'s', printString},
+{0, NULL},
 };
 int i = 0, j = 0;
 va_list obj;
@@ -40,48 +29,69 @@ va_start(obj, format);
 
 while (format[i])
 {
-  while (mind[j].cond)
-  {
-    if (mind[j].cond == format[i])
-    {
-      mind[j].printType(obj);
-      if (format[i + 1]) {
-        printf(", ");
-      }
-    }
-    j++;
-  }
-  j = 0;
-  i++;
+while (mind[j].cond)
+{
+if (mind[j].cond == format[i])
+{
+mind[j].printType(obj);
+if (format[i + 1])
+{
+printf(", ");
+}
+}
+j++;
+}
+j = 0;
+i++;
 }
 
 va_end(obj);
 printf("\n");
 }
 
+/**
+* printChar - prints a char for args
+* @lol: the arg
+* Return: void
+*/
 void printChar(va_list lol)
 {
-  printf("%c", va_arg(lol, int));
+printf("%c", va_arg(lol, int));
 }
 
+/**
+* printInt - prints an int for args
+* @lol: the arg
+* Return: void
+*/
 void printInt(va_list lol)
 {
-  printf("%d", va_arg(lol, int));
+printf("%d", va_arg(lol, int));
 }
 
+/**
+* printFloat - prints a float for args
+* @lol: the arg
+* Return: void
+*/
 void printFloat(va_list lol)
 {
-  printf("%f", va_arg(lol, double));
+printf("%f", va_arg(lol, double));
 }
 
+/**
+* printString - prints a string for args
+* @lol: the arg
+* Return: void
+*/
 void printString(va_list lol)
 {
-  char *ch;
-  ch = va_arg(lol, char *);
-  if (ch == NULL)
-  {
-  printf("(nil)");
-  return;
-  }
-  printf("%s", ch);
+char *ch;
+ch = va_arg(lol, char *);
+if (ch == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", ch);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+* struct form - the struct
+* @cond: the format
+* @printType: the func
+* Return: void
+*/
+
+typedef struct form
+{
+char cond;
+void (*printType)(va_list lol);
+} form;
+
 #endif
