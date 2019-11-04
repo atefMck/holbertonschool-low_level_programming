db445846229fc207b2bf6549e3910c53c74dfd73
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-04 13:58:55 +0100

    Advanced 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* free_list - print items in a list
+* @head: the list to be printed
+* Return: modified list
+*/
+void myStartupFun () __attribute__ ((constructor));
+
+void myStartupFun ()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
