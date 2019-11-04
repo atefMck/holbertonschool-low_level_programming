7733b5aec837d1142ade03b614c659b888d11e59
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-04 14:00:43 +0100

    Advanced 1 fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,14 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-/**
-* free_list - print items in a list
-* @head: the list to be printed
-* Return: modified list
-*/
-void myStartupFun () __attribute__ ((constructor));
 
-void myStartupFun ()
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+* myStartupFun - print items in a list
+*
+* Return: void
+*/
+void myStartupFun(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house
+upon my back!\n");
 }
