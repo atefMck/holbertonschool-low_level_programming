cb922fc1c4ad581718b2045b6411f028146b6142
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-04 13:53:05 +0100

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* free_list - print items in a list
+* @head: the list to be printed
+* Return: modified list
+*/
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
