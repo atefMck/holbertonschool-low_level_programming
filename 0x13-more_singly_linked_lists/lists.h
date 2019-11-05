28aa1f2fa0af2d016c127b153214cb53e3820ad2
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-06 00:36:40 +0100

    Advanced 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,12 +18,6 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-typedef struct listp_s
-{
-void *p;
-struct listp_s *next;
-} listp_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -40,7 +34,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 
 size_t print_listint_safe(const listint_t *head);
-void free_listp2(listp_t **head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
