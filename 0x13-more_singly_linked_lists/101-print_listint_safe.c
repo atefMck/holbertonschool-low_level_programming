bfa556bad6f5011a9a7d376d14db66e054cc53ce
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-05 23:47:25 +0100

    Testing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
-* print_listint_safe - hmmmm
-* @head: hmmmmm
-* Return: num of nodes
+* free_listp - frees a linked list
+* @head: head of a list.//Aladin Bensassi
+* Return: no return.
+*/
+void free_listp(listp_t **head)
+{
+listp_t *temp;
+listp_t *curr;
+
+if (head != NULL)
+{
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
+}
+*head = NULL;
+}
+}
+/**
+* print_listint_safe - prints a linked list.
+* @head: head of a list.//Aladin Bensassi
+* Return: number of nodes in the list.
 */
 size_t print_listint_safe(const listint_t *head)
 {
-// listint_t *temp;
-size_t c = 0, i = 0;
-
-// temp = head->next;
-while (head && i != 15)
-{
-// if (temp != head)
-// {
-printf("[%p] %d\n", (void *)head,head->n);
-head = head->next;
-// temp = head->next;
-c++;
-// }
-// else
-// {
-// printf("[%p] %d\n", (void *)head,head->n);
-// free_listint(temp);
-// }
-i++;
-}
-return (c);
-}
-
-
-/**
-* free_listint - print items in a list
-* @head: the list to be printed
-* Return: modified list
-*/
-void free_listint(listint_t *head)
-{
-listint_t *temp;
-
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
+hptr = NULL;
 while (head != NULL)
 {
-temp = head;
-head = head->next;
-free(temp);
+new = malloc(sizeof(listp_t));
+if (new == NULL)
+exit(98);
+new->p = (void *)head;
+new->next = hptr;
+hptr = new;
+add = hptr;
+while (add->next != NULL)
+{
+add = add->next;
+if (head == add->p)
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+free_listp(&hptr);
+return (nnodes);
 }
+}
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+nnodes++;
+}
+free_listp(&hptr);
+return (nnodes);
 }
