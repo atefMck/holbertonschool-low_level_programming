93ec7b8d391ac5cf52fcefea594e48a97ef35d80
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-05 23:49:01 +0100

    Testing environment

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,12 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+typedef struct listp_s
+{
+void *p;
+struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -34,5 +40,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 
 size_t print_listint_safe(const listint_t *head);
+void free_listp2(listp_t **head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
