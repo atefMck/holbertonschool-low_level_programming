d25b9258c6c02dd018d4611bee0db99a1bf2d9b3
Atef Mechken <atef.mechken@youngminds.tn>
2019-11-05 23:07:51 +0100

    Task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* delete_nodeint_at_index - hmmmm
+* @head: hmmmmm
+* @idx: hmmm
+* Return: 1 or -1
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *temp, *aux;
+if (*head == NULL)
+return (-1);
+
+temp = *head;
+if (index == 0)
+{
+*head = temp->next;
+free(temp);
+return (1);
+}
+
+index--;
+while (index != 0)
+{
+temp = temp->next;
+if (temp == NULL)
+return (-1);
+index--;
+}
+aux = temp->next;
+temp->next = aux->next;
+free(aux);
+return (1);
+}
