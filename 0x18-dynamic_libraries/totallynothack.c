138dbe4c1d926c299a2042babf481e32320d2368
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-16 21:56:32 +0100

    Uploading the library

--- 0x18-dynamic_libraries/totallynothack.c
+++ 0x18-dynamic_libraries/totallynothack.c
@@ -1,28 +1,38 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int rand()
+/**
+* rand - overwriting the rand function
+*
+* Return: 0 on success
+*/
+int rand(void)
 {
-  int number = 0;
-  number++;
-  switch (number) {
-    case 1:
-      return (9);
-      break;
-    case 2:
-      return (8);
-      break;
-    case 3:
-      return (10);
-      break;
-    case 4:
-      return (24);
-      break;
-    case 5:
-      return (75);
-      break;
-    case 6:
-      return (9);
-      break;
-    default:
-      break;
-  }
+static int number = 1;
+number++;
+switch (number)
+{
+case 2:
+return (9);
+break;
+case 3:
+return (8);
+break;
+case 4:
+return (10);
+break;
+case 5:
+return (24);
+break;
+case 6:
+return (75);
+break;
+case 7:
+return (9);
+break;
+default:
+break;
+}
 }
