a45c94f821dad402ed5a9c9b7a85c92c5eefb9e8
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-16 22:00:32 +0100

    Task 101

--- 0x18-dynamic_libraries/101-Mole/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-Mole/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/atefsama666/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/notpreloadedhack.so
-export LD_PRELOAD="/tmp/notpreloadedhack.so"
+wget -P /tmp https://raw.githubusercontent.com/atefsama666/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libnothack.so
+export LD_PRELOAD="/tmp/libnothack.so"
