cf9c922b4d15fe1cdf4649067adf4a9845208bf3
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-16 21:33:28 +0100

    Task 101

---
+++ 0x18-dynamic_libraries/totallynothack.c
@@ -0,0 +1,28 @@
+
+int rand()
+{
+  int number = 0;
+  number++;
+  switch (number) {
+    case 1:
+      return (9);
+      break;
+    case 2:
+      return (8);
+      break;
+    case 3:
+      return (10);
+      break;
+    case 4:
+      return (24);
+      break;
+    case 5:
+      return (75);
+      break;
+    case 6:
+      return (9);
+      break;
+    default:
+      break;
+  }
+}
