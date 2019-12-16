cb2f6a09c5123a6b520578038004a520ebe33308
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-16 22:17:57 +0100

    Attemt 1

--- 0x18-dynamic_libraries/totallynothack.c
+++ 0x18-dynamic_libraries/totallynothack.c
@@ -1,22 +1,31 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+* rand - overwriting the rand function
+*
+* Return: 0 on success
+*/
 int rand()
 {
-	static int ct = -1;
-
-	ct++;
-	if (ct == 0)
-		return 8;
-	if (ct == 1)
-		return 8;
-	if (ct == 2)
-		return 7;
-	if (ct == 3)
-		return 9;
-	if (ct == 4)
-		return 23;
-	if (ct == 5)
-		return 74;
-	return ct * ct % 30000;
+  static int number = 1;
+  int ab = 969;
+  number++;
+  switch (number)
+  {
+    case 2:
+    return (9);
+    case 3:
+    return (8);
+    case 4:
+    return (10);
+    case 5:
+    return (24);
+    case 6:
+    return (75);
+    case 7:
+    return (9);
+    default:
+    return (ab);
+  }
 }
