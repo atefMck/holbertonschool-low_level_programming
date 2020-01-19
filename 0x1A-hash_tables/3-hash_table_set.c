343bb1381988173f41fb5edc0a1ee0f7ec6fb967
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-19 19:47:42 +0100

    Added update key handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
 hash_node_t *newNode;
+hash_node_t *current;
 if (!key || !value || !ht || strcmp(key, "") == 0)
 return (0);
 newNode = malloc(sizeof(hash_node_t *));
@@ -19,8 +20,18 @@ return (0);
 newNode->key = strdup(key);
 newNode->value = strdup(value);
 index = key_index((const unsigned char *) key, ht->size);
-if (ht->array[index])
+current = ht->array[index];
+if (current)
 {
+while (current)
+{
+if (strcmp(current->key, key) == 0)
+{
+current->value = strdup(value);
+return (1);
+}
+current = current->next;
+}
 newNode->next = ht->array[index];
 ht->array[index] = newNode;
 }
