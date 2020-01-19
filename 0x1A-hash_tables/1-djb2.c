6e7a454f8fe925009b73931abdd9ac38c7bacec9
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-19 18:00:24 +0100

    Betty styling task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,6 +5,7 @@
 * @str: the key
 * Return: index of that key
 */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
@@ -13,7 +14,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 }
 return (hash);
 }
