240d5803b291b7221bfb16c8f953d1db01c9eed8
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-19 17:57:35 +0100

    Betty styling task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,16 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a hash table
-* @size: size of the table
-* Return: pointer to the hash table
+* hash_djb2 - hash function using djb2 algorithm
+* @str: the key
+* Return: index of that key
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+unsigned long int hash;
+int c;
+hash = 5381;
+while (c = *str++)
+hash = (((hash << 5) + hash) + c)
+return (hash);
 }
