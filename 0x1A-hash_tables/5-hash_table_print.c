21caa1c6a651f3777a7770a060132d693a63c53d
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-19 20:03:02 +0100

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table
+* @ht: the hash table
+* Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+hash_node_t *node;
+unsigned int  i;
+int s = 0;
+if (!ht || !ht->array)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+if (node)
+{
+while (node)
+{
+if (s)
+printf(", ");
+printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+s = 1;
+node = node->next;
+}
+}
+}
+printf("}\n");
+}
