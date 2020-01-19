99430de2460d7c262e3f02e1bde16b5677a95714
atefsama666 <xevious004@gmail.com>
2020-01-19 17:40:23 +0100

    Task 1 attempt 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,17 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a hash table
-* @size: size of the table
-* Return: pointer to the hash table
+* hash_djb2 - get key position
+* @str: the key
+* Return: returns key index
 */
-hash_table_t *hash_table_create(unsigned long int size)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-hash_table_t *ht;
-if (size <= 0)
-return (NULL);
-ht = malloc(sizeof(hash_table_t));
-if (!ht)
-return (NULL);
-ht->size = size;
-ht->array = malloc(sizeof(hash_node_t *) * size);
-if (!ht->array)
-{
-free(ht);
-return (NULL);
-}
-return (ht);
+unsigned long hash = 5381;
+int c;
+
+while (c = *str++)
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+return hash;
 }
