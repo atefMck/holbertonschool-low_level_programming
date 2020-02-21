5906eebcb7215058027540fec36886a193456f23
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-21 23:02:37 +0100

    Task 5 fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,15 +9,12 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                try:
-	                if grid[x + 1][y] == 0:
-	                    perim += 1
-	                if grid[x - 1][y] == 0:
-	                    perim += 1
-	                if grid[x][y + 1] == 0:
-	                    perim += 1
-	                if grid[x][y - 1] == 0:
-	                    perim += 1
-                except IndexError:
+                if x + 1 >= len(grid) or grid[x + 1][y] == 0:
+                    perim += 1
+                if x - 1 < 0 or grid[x - 1][y] == 0:
+                    perim += 1
+                if y + 1 >= len(grid[x]) or grid[x][y + 1] == 0:
+                    perim += 1
+                if y - 1 < 0 or grid[x][y - 1] == 0:
                     perim += 1
     return (perim)
