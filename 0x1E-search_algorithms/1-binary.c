b1365fb3de523a97ca8f21b3afe5b7b2c5ea76da
atefsama666 <mechken.atef@gmail.com>
2020-05-27 13:31:13 +0100

    Task 2

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in array
+ * @array: input array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of first encounter
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t end = size;
+	size_t start = 0;
+	size_t mid;
+	size_t i;
+	int piv;
+
+	if (!array)
+		return (-1);
+	while (start < end)
+	{
+		printf("Searching in array: ");
+		for (i = start; i < end - 1; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		mid = (start + end) / 2;
+		piv = array[mid];
+		if (value == piv)
+			return (mid);
+		else if (piv > value)
+			end = mid - 1;
+		else if (piv <= value)
+			start = mid + 1;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,5 @@
 #ifndef SEARCH_HEAD
 #define SEARCH_HEAD
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
