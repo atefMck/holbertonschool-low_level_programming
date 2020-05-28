a82d2d97e8d9e27fdf09266cf53e817fa82e736c
atefsama666 <mechken.atef@gmail.com>
2020-05-28 11:42:51 +0100

    Task 102

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in array
+ * @array: input array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of first encounter
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int high = size - 1;
+	int low = 0;
+	int pos;
+	int piv;
+
+	if (!array)
+		return (-1);
+	while (high >= low && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+		 * (value - array[low]));
+		piv = array[pos];
+		printf("Value checked array[%d] = [%d]\n", pos, piv);
+		if (value == piv)
+			return (pos);
+		else if (piv > value)
+			high = pos;
+		else if (piv < value)
+			low = pos + 1;
+	}
+	printf("Value checked array[%d] is out of range\n", pos);
+	return (-1);
+}
