490b2c76d7bf6103bbaecccc63c25b0a17e5146c
atefsama666 <mechken.atef@gmail.com>
2020-05-28 11:45:40 +0100

    Task 102 betty styled

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	int high = size - 1;
 	int low = 0;
 	int pos = low + (((double)(high - low) / (array[high] - array[low]))
-		 * (value - array[low]));;
+		 * (value - array[low]));
 	int piv;
 
 	if (!array)
