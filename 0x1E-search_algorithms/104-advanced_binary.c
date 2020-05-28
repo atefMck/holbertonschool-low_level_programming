89fb803ed9095f216fe573e7ea9be522f659d634
atefsama666 <mechken.atef@gmail.com>
2020-05-28 13:27:42 +0100

    Task 104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -15,24 +15,21 @@ int rec_binary(int *array, size_t start, size_t end, int value)
 	size_t mid;
 	size_t i;
 
-	if (end < start)
+	if (end <= start)
 		return (-1);
-	while (end > start)
-	{
-		printf("Searching in array: ");
-		for (i = start; i < end - 1; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-		mid = (start + end - 1) / 2;
-		if (value == array[mid] && array[mid - 1] != value)
-			return (mid);
-		else if (array[mid] > value)
-			return (rec_binary(array, start, mid, value));
-		else if (array[mid] < value)
-			return (rec_binary(array, mid + 1, end, value));
-		else
-			return (rec_binary(array, start, mid, value));
-	}
+	printf("Searching in array: ");
+	for (i = start; i < end - 1; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+	mid = (start + end - 1) / 2;
+	if (value == array[mid] && array[mid - 1] != value)
+		return (mid);
+	else if (array[mid] > value)
+		return (rec_binary(array, start, mid, value));
+	else if (array[mid] < value)
+		return (rec_binary(array, mid + 1, end, value));
+	else
+		return (rec_binary(array, start, mid, value));
 	return (-1);
 }
 
